438f8d8944dde1812ad60204dde564d5152369ed
myshuker <myshuker@gmai.com>
2020-12-11 23:43:42 +0100

    add GoBack button

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const quiz = document.getElementById("quiz");
 const answerA = document.getElementById("answerA");
 const answerB = document.getElementById("answerB");
 const answerC = document.getElementById("answerC");
-
-const Questions = [];
+const goBack = document.getElementById("goBack");
 
 // create Buttons days
 for (let i = 1; i <= 31; i++) {
@@ -27,87 +26,88 @@ for (let i = 1; i <= 31; i++) {
   showResults = () => {
     document.getElementById("content-page").style.display = "none"; // to hide all the elements page
 
-    // console.log("number" + i);
+    let goBackButton = document.createElement("button");
+    goBackButton.className = "  btn-secondary";
+    goBackButton.innerText = "Go Back";
+    goBackButton.setAttribute("disabled", true);
+    goBack.appendChild(goBackButton);
+
+    goBack.addEventListener("click", () => {
+      document.getElementById("content-page").style.display = "block"; // to show all the elements page
+      document.getElementById("question").style.display = "none"; // to hide all the question section
+      button.setAttribute("disabled", true);
+    });
+
     const randomNumber = Math.round(Math.random() * 41);
-    // console.log(randomNumber);
 
     fetch("./questions.json")
       .then((res) => res.json())
       .then((data) => {
-        Questions.splice(0, Questions.length); //remove all elements from the array and clean the original array.
-        Questions.push(data);
-        // console.log(Questions);
-        const question = Questions[0][randomNumber].question;
+        const question = data[randomNumber].question;
+        const answer1 = data[randomNumber].answers.a;
+        const answer2 = data[randomNumber].answers.b;
+        const answer3 = data[randomNumber].answers.c;
+        const correctAnswer = data[randomNumber].correctAnswer;
+
         quiz.innerHTML = question;
-        // console.log(quiz);
-
-        const answer1 = Questions[0][randomNumber].answers.a;
-        const answer2 = Questions[0][randomNumber].answers.b;
-        const answer3 = Questions[0][randomNumber].answers.c;
-
-        const correctAnswer = Questions[0][randomNumber].correctAnswer;
-        console.log(correctAnswer);
-
         answerA.innerHTML = answer1;
-        console.log(answerA);
         answerB.innerHTML = answer2;
         answerC.innerHTML = answer3;
 
+        // add action to answers buttons
         answerA.addEventListener("click", () => {
+          goBackButton.removeAttribute("disabled");
           if (answer1 == correctAnswer) {
             alert("Your answer is correct");
             answerA.className = "btn-success";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
-            // button.setAttribute("disabled", true);
           } else {
             answerA.className = "btn-danger";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
             alert("Your answer is wrong");
-            // button.setAttribute("disabled", true);
           }
         });
 
         answerB.addEventListener("click", () => {
+          goBackButton.removeAttribute("disabled");
           if (answer2 == correctAnswer) {
             alert("Your answer is correct");
             answerB.className = "btn-success";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
-            // button.setAttribute("disabled", true);
           } else {
             answerB.className = "btn-danger";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
             alert("Your answer is wrong");
-            // button.setAttribute("disabled", true);
           }
         });
 
         answerC.addEventListener("click", () => {
+          goBackButton.removeAttribute("disabled");
           if (answer3 == correctAnswer) {
             alert("Your answer is correct");
             answerC.className = "btn-success";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
-            // button.setAttribute("disabled", true);
           } else {
             answerC.className = "btn-danger";
             answerA.setAttribute("disabled", true);
             answerB.setAttribute("disabled", true);
             answerC.setAttribute("disabled", true);
             alert("Your answer is wrong");
-            // button.setAttribute("disabled", true);
           }
         });
       });
   };
 
+  // add action to days buttons
   button.addEventListener("click", showResults);
 }
