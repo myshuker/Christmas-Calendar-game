7abb072baddface9042c199a3467de3e8de6c1e8
myshuker <myshuker@gmai.com>
2020-12-11 16:36:08 +0100

    add correct code

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // select these HTML elements
+const numberContainer = document.getElementById("number-container");
 const quiz = document.getElementById("quiz");
-const submit = document.getElementById("submit");
 const answerA = document.getElementById("answerA");
 const answerB = document.getElementById("answerB");
 const answerC = document.getElementById("answerC");
-const results = document.getElementById("results");
-const numberContainer = document.getElementById("number-container");
 
 const Questions = [];
-let answer1 = "";
-let answer2 = "";
-let answer3 = "";
 
 // create Buttons days
 for (let i = 1; i <= 31; i++) {
@@ -21,6 +16,7 @@ for (let i = 1; i <= 31; i++) {
 
   //create Results handler
   showResults = () => {
+
     // location.reload();
     //redirect to new Page
     // window.location = "./answers.html";
@@ -32,7 +28,7 @@ for (let i = 1; i <= 31; i++) {
     fetch("./questions.json")
       .then((res) => res.json())
       .then((data) => {
-        // Questions.splice(0, Questions.length); //remove all elements from the array and clean the original array.
+        Questions.splice(0, Questions.length); //remove all elements from the array and clean the original array.
         Questions.push(data);
         console.log(Questions);
         const question = Questions[0][randomNumber].question;
@@ -46,93 +42,66 @@ for (let i = 1; i <= 31; i++) {
         const correctAnswer = Questions[0][randomNumber].correctAnswer;
         console.log(correctAnswer);
 
-        // let buttonA = document.createElement("button");
-        // let buttonB = document.createElement("button");
-        // let buttonC = document.createElement("button");
-        // buttonA.className = " btn btn-secondary";
-        // buttonB.className = " btn btn-secondary";
-        // buttonC.className = " btn btn-secondary";
-
         answerA.innerHTML = answer1;
         console.log(answerA);
         answerB.innerHTML = answer2;
         answerC.innerHTML = answer3;
 
-        // quiz.appendChild(quiz);
-        // answerA.appendChild(buttonA);
-        // answerB.appendChild(buttonB);
-        // answerC.appendChild(buttonC);
-
-        // let submitButton = document.createElement("submit");
-        // submitButton.className = "btn btn-warning";
-        // submitButton.innerText = "Submit";
-        // submit.appendChild(submitButton);
-
         answerA.addEventListener("click", () => {
           if (answer1 == correctAnswer) {
-            // alert("this correct");
+            alert("Your answer is correct");
             answerA.className = "btn-success";
+            answerB.setAttribute("disabled", true);
+            answerC.setAttribute("disabled", true);
+            button.setAttribute("disabled", true);
+
           } else {
             answerA.className = "btn-danger";
-            // alert("this wrong");
+            answerB.setAttribute("disabled", true);
+            answerC.setAttribute("disabled", true);
+            alert("Your answer is wrong");
+            button.setAttribute("disabled", true);
+
           }
         });
 
         answerB.addEventListener("click", () => {
           if (answer2 == correctAnswer) {
-            // alert("this correct");
+            alert("Your answer is correct");
             answerB.className = "btn-success";
+            answerA.setAttribute("disabled", true);
+            answerC.setAttribute("disabled", true);
+            button.setAttribute("disabled", true);
+
           } else {
             answerB.className = "btn-danger";
-            // alert("this wrong");
+            answerA.setAttribute("disabled", true);
+            answerC.setAttribute("disabled", true);
+            alert("Your answer is wrong");
+            button.setAttribute("disabled", true);
+
           }
         });
 
         answerC.addEventListener("click", () => {
           if (answer3 == correctAnswer) {
-            // alert("this correct");
+            alert("Your answer is correct");
             answerC.className = "btn-success";
+            answerB.setAttribute("disabled", true);
+            answerA.setAttribute("disabled", true);
+            button.setAttribute("disabled", true);
+
           } else {
             answerC.className = "btn-danger";
-            // alert("this wrong");
+            answerB.setAttribute("disabled", true);
+            answerA.setAttribute("disabled", true);
+            alert("Your answer is wrong");
+            button.setAttribute("disabled", true);
+
           }
         });
       });
   };
 
   button.addEventListener("click", showResults);
-}
-
-//this code down not in use and need deleted in the end
-
-// fetching questions from json :
-
-function buildRandomQuiz() {}
-
-function showResults() {}
-
-// display quiz right away
-buildRandomQuiz();
-
-// on submit, show results
-// submitButton.addEventListener("click", showResults);
-
-// Step 1 Questions :
-
-// fetching questions from json :
-
-fetch("./questions.json")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-
-    console.log(Questions);
-    // Questions.push(data);
-    console.log(Questions);
-  });
-
-// Step 2 create functions:
-function buildQuiz() {
-  // variable to store the HTML output
-  const output = [];
 }
